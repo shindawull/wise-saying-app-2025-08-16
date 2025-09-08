66f8209d156f5e81b052a29fe250801ae98de2ea
shindawull <juoiy05@naver.com>
2025-09-08 19:02:42 +0900

    work TDD green

--- src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,4 +1,51 @@
 package com.dawull.domain.wiseSaying.controller;
 
+import com.dawull.App;
+import com.dawull.standard.util.TestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThat;
+
 public class WiseSayingControllerTest {
+    @Test
+    @DisplayName("== 명언 앱 ==")
+    public void t1() {
+        Scanner scanner = TestUtil.getScanner("종료");
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output).contains("== 명언 앱 ==");
+
+    }
+
+    @Test
+    @DisplayName("명령) ")
+    public void t2() {
+        Scanner scanner = TestUtil.getScanner("""
+                목록
+                종료
+                """);
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output).contains("명령) ");
+    }
+
 }
