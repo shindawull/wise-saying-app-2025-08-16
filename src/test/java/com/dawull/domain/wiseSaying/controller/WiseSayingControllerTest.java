1ec6f97acf63b8c209e4f79a15d751434a0eb458
shindawull <juoiy05@naver.com>
2025-09-10 03:58:26 +0900

    work TDD refactoring

--- src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -17,7 +17,7 @@ public class WiseSayingControllerTest {
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
-        String output = AppTest.run("종료");
+        String output = AppTest.run("");
 
         assertThat(output).contains("== 명언 앱 ==");
 
@@ -28,7 +28,6 @@ public class WiseSayingControllerTest {
     public void t2() {
         String output = AppTest.run("""
                 목록
-                종료
                 """);
 
         assertThat(output)
@@ -41,7 +40,6 @@ public class WiseSayingControllerTest {
         String output = AppTest.run("""
                 목록
                 목록
-                종료
                 """);
 
         String[] split = output.split("명령\\)");
