1b52defd9675e7f07416f3a6d6742b41f14c0707
shindawull <juoiy05@naver.com>
2025-09-10 04:00:24 +0900

    work TDD red

--- src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -46,5 +46,19 @@ public class WiseSayingControllerTest {
         assertThat(split).hasSize(4);
     }
 
+    @Test
+    @DisplayName("등록을 입력하면 내용과 작가를 입력받는다.")
+    public void t4() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """);
+
+        assertThat(output)
+                .contains("명언 : ")
+                .contains("작가 : ");
+    }
+
 
 }
