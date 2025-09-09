584660cd03543f27fa1a74223f9b8c425cad8da8
shindawull <juoiy05@naver.com>
2025-09-10 03:45:40 +0900

    work TDD red

--- src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -1,6 +1,7 @@
 package com.dawull.domain.wiseSaying.controller;
 
 import com.dawull.AppTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 //import static org.assertj.core.api.Assertions.assertThat;
 
 public class WiseSayingControllerTest {
+    @BeforeEach
+    void beforeEach() {
+        //AppTest.clear();
+    }
+
     @Test
     @DisplayName("== 명언 앱 ==")
     public void t1() {
@@ -28,5 +34,19 @@ public class WiseSayingControllerTest {
         assertThat(output)
                 .contains("명령) ");
     }
+
+    @Test
+    @DisplayName("명령을 2번 이상 입력할 수 있습니다.")
+    public void t3() {
+        String output = AppTest.run("""
+                목록
+                목록
+                종료
+                """);
+
+        String[] split = output.split("명령\\)");
+        assertThat(output).hasSize(4);
+    }
+
 
 }
--- src/test/java/com/dawull/standard/util/TestUtil.java
+++ src/test/java/com/dawull/standard/util/TestUtil.java
@@ -4,10 +4,13 @@ import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
+    // gen == generate 생성하닥.
+    // 테스트용 스캐너 생성
     public static Scanner genScanner(String input) {
         return new Scanner(input);
     }
 
+    // System.out의 출력을 스트림으로 받기
     public static ByteArrayOutputStream setOutToByteArray() {
         ByteArrayOutputStream output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
@@ -15,6 +18,7 @@ public class TestUtil {
         return output;
     }
 
+    // setOutToByteArray 함수의 사용을 완료한 후 정리하는 함수, 출력을 다시 정상화 하는 함수
     public static void clearSetOutToByteArray(ByteArrayOutputStream byteArrayOutputStream) {
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
         try {
