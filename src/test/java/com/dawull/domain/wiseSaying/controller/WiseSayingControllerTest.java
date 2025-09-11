7ca9d7c63e3e6cb73b8222a30573a6462677a891
shindawull <juoiy05@naver.com>
2025-09-12 05:00:48 +0900

    work TDD red

--- src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/dawull/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -68,11 +68,15 @@ public class WiseSayingControllerTest {
                 등록
                 과거에 집착하지 마라.
                 작자미상
+                등록
+                나의 죽음을 적들에게 알리지 말라!
+                이순신
                 목록
                 """);
 
         assertThat(output)
                 .contains("번호 / 작가 / 명언")
+                .contains("3 / 이순신 / 나의 죽음을 적들에게 알리지 마라!")
                 .contains("2 / 작자미상 / 과거에 집착하지 마라.")
                 .contains("1 / 작자미상 / 현재를 사랑하라.");
     }
